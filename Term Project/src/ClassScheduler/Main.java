d6bf22efa81bd6d7f0a3af271b8a86e01fdeaf32
Bdpelc <bdpelc@gmail.com>
2013-10-02 16:20:39 -0500

    Changed the output file directory to point at workspace folder.

--- Term Project/src/ClassScheduler/Main.java
+++ Term Project/src/ClassScheduler/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		ExcelInput testInput = new ExcelInput();
 		testInput.setInputFile("schedule.xls");
 		testInput.read();
-		OutputFile file = new OutputFile("C:\\Users\\CraigNetbook\\Desktop\\test.txt");
+		OutputFile file = new OutputFile("test.txt");
 		for (int i=1;i<testInput.rowCount();i++){
 			Section section = new Section(testInput.passRow(i));
 			file.writeString("Class Code: "+section.getClassCode());
